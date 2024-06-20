052c646744023e88080bfef0849251bbad4334d0
D.A. Marcyes <91547826+dathegreat@users.noreply.github.com>
2024-06-20 11:34:00 -0600

    Adjusts path again to try and avoid github pages weirdness

--- main.js
+++ main.js
@@ -29,6 +29,6 @@ function generatePaths(basePath, count) {
 var basePath = '/test';
 var numberOfPaths = 100;
 var paths = generatePaths(basePath, numberOfPaths);
-setCookie('testCookie', 'testValue', window.location.hostname + basePath, basePath, new Date('2024-12-31'), false, false);
+setCookie('testCookie', 'testValue', window.location.hostname, basePath, new Date('2024-12-31'), false, true);
 generateNetworkTraffic(paths);
 document.body.innerHTML = "<h1>Network traffic generated. Fetched ".concat(numberOfPaths, " paths. Check the console for details.</h1>");
--- main.ts
+++ main.ts
@@ -35,6 +35,6 @@ const basePath = '/test';
 const numberOfPaths = 100;
 const paths = generatePaths(basePath, numberOfPaths);
 
-setCookie('testCookie', 'testValue', window.location.hostname + basePath, basePath, new Date('2024-12-31'), false, false);
+setCookie('testCookie', 'testValue', window.location.hostname, basePath, new Date('2024-12-31'), false, true);
 generateNetworkTraffic(paths);
 document.body.innerHTML = `<h1>Network traffic generated. Fetched ${numberOfPaths} paths. Check the console for details.</h1>`;
